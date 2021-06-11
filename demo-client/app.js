7f02a450f77d7f8be2ea3145955374d5198a75f5
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-11 09:21:38 -0600

    Add static demo streaming client

---
+++ demo-client/app.js
@@ -0,0 +1,14 @@
+function blobToPngBase64(blob) {
+  const imageBlob = new Blob([blob], {type: 'image/png'});
+  return URL.createObjectURL(imageBlob);
+}
+
+function startStreaming() {
+  const pngStreamElement = document.querySelector("#png-stream")
+  const socket = new WebSocket("ws://localhost:9002");
+  socket.onmessage = async function (event) {
+    pngStreamElement.src = blobToPngBase64(event.data);
+  }
+}
+
+window.addEventListener('load', (_) => startStreaming());
